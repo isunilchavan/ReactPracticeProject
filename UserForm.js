d5476d84d53dcf3f51ab78d07ab7dd3e6e271ad1
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-11 12:21:37 +0530

    useReducer

--- UserForm.js
+++ UserForm.js
@@ -1,46 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import "./UserForm.css";
 import ErrorModal from "./ErrorModal";
-// import UserList from "./UserList";
+
+const initialState = {
+  username: "",
+  age: "",
+  college: "",
+  errorMessage: "",
+  isErrorModalOpen: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setUsername":
+      return { ...state, username: action.payload };
+    case "setAge":
+      return { ...state, age: action.payload };
+    case "setCollege":
+      return { ...state, college: action.payload };
+    case "setErrorMessage":
+      return { ...state, errorMessage: action.payload };
+    case "setIsErrorModalOpen":
+      return { ...state, isErrorModalOpen: action.payload };
+    case "resetForm":
+      return { ...initialState }; // Reset the form fields
+    default:
+      return state;
+  }
+};
 
 function UserForm({ addUser }) {
-  const [username, setUsername] = useState("");
-  const [age, setAge] = useState("");
-  const [college, setCollege] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
-  const [users, setUsers] = useState([]);
-
-  // Load user data from local storage when the component mounts
-  useEffect(() => {
-    const storedUsers = JSON.parse(localStorage.getItem("users") || "[]");
-    setUsers(storedUsers);
-  }, []);
-
-  // Save user data to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem("users", JSON.stringify(users));
-  }, [users]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleAddUser = () => {
+    const { username, age, college } = state;
+
     if (!username.trim() || !age.trim() || !college.trim()) {
-      setErrorMessage("Invalid input. All fields are required.");
-      setIsErrorModalOpen(true);
+      dispatch({ type: "setErrorMessage", payload: "Invalid input. All fields are required." });
+      dispatch({ type: "setIsErrorModalOpen", payload: true });
     } else if (parseInt(age) < 0) {
-      setErrorMessage("Invalid age. Age must be greater than 0.");
-      setIsErrorModalOpen(true);
+      dispatch({ type: "setErrorMessage", payload: "Invalid age. Age must be greater than 0." });
+      dispatch({ type: "setIsErrorModalOpen", payload: true });
     } else {
-      const newUser = { username, age: parseInt(age), college };
-      setUsers([...users, newUser]);
-      setUsername("");
-      setAge("");
-      setCollege("");
-      setErrorMessage("");
+      addUser({ username, age: parseInt(age), college });
+      dispatch({ type: "resetForm" }); // Reset the form fields
+      dispatch({ type: "setErrorMessage", payload: "" });
     }
   };
 
   const handleCloseErrorModal = () => {
-    setIsErrorModalOpen(false);
+    dispatch({ type: "setIsErrorModalOpen", payload: false });
   };
 
   return (
@@ -48,34 +57,33 @@ function UserForm({ addUser }) {
       <input
         type="text"
         placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        value={state.username}
+        onChange={(e) => dispatch({ type: "setUsername", payload: e.target.value })}
         className="input-field"
       />
       <input
         type="number"
         placeholder="Age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
+        value={state.age}
+        onChange={(e) => dispatch({ type: "setAge", payload: e.target.value })}
         className="input-field"
       />
       <input
         type="text"
         placeholder="College Name"
-        value={college}
-        onChange={(e) => setCollege(e.target.value)}
+        value={state.college}
+        onChange={(e) => dispatch({ type: "setCollege", payload: e.target.value })}
         className="input-field"
       />
       <button onClick={handleAddUser} className="add-button">
         Add User
       </button>
-      {isErrorModalOpen && (
+      {state.isErrorModalOpen && (
         <ErrorModal
-          errorMessage={errorMessage}
+          errorMessage={state.errorMessage}
           onClose={handleCloseErrorModal}
         />
       )}
-      {/* <UserList users={users} /> */}
     </div>
   );
 }
